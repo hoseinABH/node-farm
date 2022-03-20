0000b88e31110b54479da8d64fbb00846ed3c0db
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-03-20 12:06:10 +0330

    feat:render overview template

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -24,7 +40,15 @@ const app = http.createServer((req, res) => {
 
   // Overview
   if (pathName === '/overvivew' || pathName === '/') {
-    res.end('Hello From Overview');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
 
     // Product
   } else if (pathName === '/product') {
